fefe76f9c473fe58cd8fd44f59b61ac30cc83923
Alena Nikulina <alenanikulina0@gmail.com>
2020-05-20 11:33:58 -0400

    finished game functionality

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const square = document.querySelectorAll('.square')
 const mole = document.querySelectorAll('.mole')
-const timeLeft = document.querySelector('.time-left')
+const timeLeft = document.querySelector('#time-left')
 let score = document.querySelector('#score')
 
 let result = 0
+let currentTime = timeLeft.textContent
 
+// function to make the mole appear in the random squares
 function randomSquare() {
     square.forEach(className => {
         className.classList.remove('mole')
@@ -12,21 +14,36 @@ function randomSquare() {
     let randomPosition = square[Math.floor(Math.random() * 9)]
     randomPosition.classList.add('mole')
 
-    // assign the id of the randomPosition to hitPosition for us to use later
+    //assign the id of the randomPosition to hitPosition for us to use later
     hitPosition = randomPosition.id
 }
 
 square.forEach(id => {
     id.addEventListener('mouseup', () => {
-        if(id.id === hitPosition){
+        if (id.id === hitPosition) {
             result = result + 1
             score.textContent = result
+            hitPosition = null
         }
     })
 })
 
 function moveMole() {
-    let timerId = nulltimerId = setInterval(randomSquare, 1000)
+    let timerId = null
+    timerId = setInterval(randomSquare, 500)
 }
 
 moveMole()
+
+// countdowm for the timer
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if (currentTime === 0) {
+        clearInterval(timerId)
+        alert('GAME OVER! Your final score is' + result)
+    }
+}
+
+let timerId = setInterval(countDown, 1000)
